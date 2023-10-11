8bbc089eed6d8bbddb888cdb67d90f6ed9ae13bc
griff <griffw@students.cs.ubc.ca>
2023-10-11 15:03:31 -0700

    Phase 1 of CPSC210 project:
    Completed implementations of methods.
    
    TODO: Complete main function

--- src/test/model/ScheduleTest.java
+++ src/test/model/ScheduleTest.java
@@ -99,12 +99,86 @@ class ScheduleTest {
         testSchedule.addExercise(e3, DayType.MONDAY);
         testSchedule.addExercise(e5, DayType.MONDAY);
         testSchedule.removeExercise(e1, DayType.MONDAY);
+        testSchedule.removeExercise(e6, DayType.MONDAY);
 
         assertEquals(2, testSchedule.getExercises(DayType.MONDAY).size());
         assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e1));
 
+    }
 
+    @Test
+    void testSwapDiffDays() {
+
+        testSchedule.addExercise(e1, DayType.MONDAY);
+        testSchedule.addExercise(e2, DayType.TUESDAY);
+
+        testSchedule.swapExerciseDays(DayType.MONDAY, DayType.TUESDAY);
+
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e2));
+        assertTrue(testSchedule.getExercises(DayType.TUESDAY).contains(e1));
+        assertEquals(1, testSchedule.getExercises(DayType.MONDAY).size());
+        assertEquals(1, testSchedule.getExercises(DayType.TUESDAY).size());
 
     }
 
+    @Test
+    void testSwapDiffDaysMultipleExercises() {
+        testSchedule.addExercise(e1, DayType.MONDAY);
+        testSchedule.addExercise(e2, DayType.MONDAY);
+        testSchedule.addExercise(e6, DayType.MONDAY);
+
+        testSchedule.addExercise(e4, DayType.TUESDAY);
+        testSchedule.addExercise(e5, DayType.TUESDAY);
+
+        testSchedule.swapExerciseDays(DayType.MONDAY, DayType.TUESDAY);
+
+        //CHECK MONDAY
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e4));
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e5));
+        assertEquals(2, testSchedule.getExercises(DayType.MONDAY).size());
+        assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e1));
+        assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e2));
+        assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e6));
+
+
+        //CHECK TUESDAY
+        assertTrue(testSchedule.getExercises(DayType.TUESDAY).contains(e1));
+        assertTrue(testSchedule.getExercises(DayType.TUESDAY).contains(e2));
+        assertEquals(3, testSchedule.getExercises(DayType.TUESDAY).size());
+        assertTrue(testSchedule.getExercises(DayType.TUESDAY).contains(e6));
+        assertFalse(testSchedule.getExercises(DayType.TUESDAY).contains(e4));
+        assertFalse(testSchedule.getExercises(DayType.TUESDAY).contains(e5));
+
+    }
+
+    @Test
+    void testSwapSameDays() {
+        testSchedule.addExercise(e1, DayType.MONDAY);
+        testSchedule.addExercise(e2, DayType.MONDAY);
+        testSchedule.addExercise(e3, DayType.MONDAY);
+
+        assertEquals(3, testSchedule.getExercises(DayType.MONDAY).size());
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e1));
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e2));
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e3));
+        assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e4));
+
+        testSchedule.swapExerciseDays(DayType.MONDAY, DayType.MONDAY);
+
+        //NOTHING HAPPENS, SAME TESTS
+        assertEquals(3, testSchedule.getExercises(DayType.MONDAY).size());
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e1));
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e2));
+        assertTrue(testSchedule.getExercises(DayType.MONDAY).contains(e3));
+        assertFalse(testSchedule.getExercises(DayType.MONDAY).contains(e4));
+    }
+
+    @Test
+    void ModifyCardioExercise(){
+    }
+
+    @Test
+    void ModifyBodyWeightExercise(){}
+
 }
+
