7e41b544376b73e366c475fd0708bd18da58a23d
griff <griffw@students.cs.ubc.ca>
2023-10-13 09:53:54 -0700

    Phase 1 of CPSC210 project:
    Additional test coverage

--- src/test/model/ExerciseTest.java
+++ src/test/model/ExerciseTest.java
@@ -3,25 +3,52 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExerciseTest {
 
-    Exercise exercise;
+    Exercise cardioExercise;
+    Exercise bodyWeightExercise;
 
     @BeforeEach
     void runBefore() {
 
-        exercise = new Exercise("Running", 0, 0, 10);
+        cardioExercise = new Exercise("Running", 0, 0, 10);
+        bodyWeightExercise = new Exercise("Pushups", 3, 12, 0);
 
     }
 
     @Test
     void testExerciseConstructor() {
-        assertEquals("Running", exercise.getExerciseName());
-        assertEquals(10, exercise.getDuration());
-        assertEquals(0, exercise.getSets());
-        assertEquals(0, exercise.getReps());
+        assertEquals("Running", cardioExercise.getExerciseName());
+        assertEquals(10, cardioExercise.getDuration());
+        assertEquals(0, cardioExercise.getSets());
+        assertEquals(0, cardioExercise.getReps());
+        assertFalse(cardioExercise.getIsCompleted());
+    }
+
+    @Test
+    void testSetReps() {
+        bodyWeightExercise.setReps(10);
+        assertEquals(10, bodyWeightExercise.getReps());
+    }
+
+    @Test
+    void testSetSets() {
+        bodyWeightExercise.setSets(4);
+        assertEquals(4, bodyWeightExercise.getSets());
+    }
+
+    @Test
+    void testSetDuration() {
+        cardioExercise.setDuration(20);
+        assertEquals(20, cardioExercise.getDuration());
+    }
+
+    @Test
+    void testSetCompletionStatus() {
+        cardioExercise.setCompletionStatus(true);
+        assertTrue(cardioExercise.getIsCompleted());
     }
 
 }
